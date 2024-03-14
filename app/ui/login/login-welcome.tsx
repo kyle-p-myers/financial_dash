456717b80f4f70db7e3caf778641e3cd02c2c585
kyle-p-myers <kmyers2438@gmail.com>
2024-03-14 16:37:15 -0600

    Create note updates

--- app/ui/login/login-welcome.tsx
+++ app/ui/login/login-welcome.tsx
@@ -3,15 +3,15 @@ import React from 'react';
 
 export default function LoginWelcome() {
   return (
-    <div className='flex flex-col items-center justify-center h-full' style={{ minHeight: '100vh', minWidth: '50vw',
+    <div className='flex flex-col items-center justify-center' style={{ minHeight: '100vh', minWidth: '50vw',
     background: 'linear-gradient(to bottom, #c94b4b, #4b134f)'}}
 >
         <div className=' text-white'>
-          <h1 className='flex flex-col text-5xl'>Welcome to Pidge!</h1>
+          <h1 className='flex text-5xl m '>Welcome to Pidge!</h1>
           <h3>Document Faster. Learn Faster</h3>
         </div>
-        <div className=' text-2xl text-white'>
-          <p className=' flex flex-col'>
+        <div className=' flex flex-col text-2xl text-white'>
+          <p className=' flex'>
             Create Document flows without <br /> ever leaving your browser
           </p>
           <p className='flex flex-col'>Reduce onboarding time</p>
