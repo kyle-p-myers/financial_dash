2dd4ce39cf36795e454680455932ef5d7a480968
Kyle Myers <kmyers2438@gmail.com>
2024-04-27 18:05:40 -0600

    addded notes table

--- app/api/create-user-table/route.ts
+++ app/api/create-user-table/route.ts
@@ -4,9 +4,11 @@ import { NextResponse } from 'next/server';
 export async function GET(request: Request) {
   try {
     const result =
-      await sql`CREATE TABLE Users ( Name varchar(255), email varchar(255), id integer );`;
+      await sql`CREATE TABLE Users ( Name varchar(255), email varchar(255), id varchar(255) ); `;
+      await sql `CREATE TABLE Notes (id varchar(255), title varchar(255), body varchar(255), priority varchar (255)); `;
     return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
 }
+
